317ffbc48521dad7f7ece224e5584904b8bdd9fe
Rajitha34 <chowdaryrajitha08@gmail.com>
2022-01-24 18:50:42 +0530

    task added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app id="inspire">
      <v-navigation-drawer 
+     color=""
      v-model="drawer"
      app>
-      <v-list-item>
+      <v-list-item >
         <v-list-item-content>
           <v-list-item-title class="text-h6">
             Vue Todo
@@ -37,16 +38,55 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar
+       app
+      color="teal"
+      dense
+      dark
+      prominent
+    >
+      <v-app-bar-nav-icon @click="drawer= !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>ALL TASK</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-checkbox-blank-badge-outline</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-horizontal-circle-outline</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 3"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Task {{ n }}</v-list-item-title>
+            
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
+  
 </template>
 
 <script>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,85 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo page</h1>
-  </div>
+<div class="home ">
+
+    <v-list
+     flat
+     >
+       <div 
+       v-for="task in tasks"
+        :key="task.id">
+        <v-list-item @click="doneTask(task.id)"
+        :class="{'blue lighten-5':task.done}">
+            <template v-slot:default>
+                <v-list-item-action>
+                  
+                    <v-checkbox :input-value="task.done" color="teal"></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                    <v-list-item-title
+                    :class="{'text-decoration-line-through' : task.done }"
+                    >
+                    {{task.title}}
+                    </v-list-item-title>
+                    
+                </v-list-item-content>
+                  <v-list-item-action>
+          <v-btn
+           @click.stop="deleteTask(task.id)" 
+          icon
+         >
+            
+            <v-icon color="teal">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+
+            </template>
+            
+        </v-list-item>
+       </div>
+    </v-list>
+</div>
 </template>
 
 <script>
-
-  export default {
+export default {
     name: 'Home',
+    data() {
+        return {
+          tasks: [
+            {
+              id: 1,
+              title:'Today',
+              done: false
+            },
+            {
+              id: 2,
+              title:'Tomorrow',
+              done: false
+            },
+            {
+              id: 3,
+              title:'Up coming',
+              done: false
+            },
+            {
+              id: 4,
+              title:'Someday',
+              done: false
+            },
+          ]
 
-    
-  }
+        }
+    },
+    methods:{
+      doneTask(id) {
+        let task = this.tasks.filter(task => task.id === id)[0]
+        task.done = !task.done
+      },
+      deleteTask(id) {
+        this.tasks = this.tasks.filter(task => task.id !== id)
+      }
+    }
+
+}
 </script>
- 
