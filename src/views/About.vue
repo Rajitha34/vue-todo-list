1fe37d5710756f2b242ba33e51949af206eb1933
Rajitha34 <chowdaryrajitha08@gmail.com>
2022-01-27 21:30:24 +0530

    added img

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>About Vue Todo</h1>
-  </div>
+  <v-main>
+    <div class="about">
+      <v-img src="../assets/img6.jpg" height="200px">
+      <v-row align="end" class="lightbox white--text pa-2 fill-height">
+        <v-col>
+          <v-container>
+            <div class="headline"> </div>
+          </v-container>
+
+        </v-col>
+      </v-row>
+      
+      </v-img>
+
+    </div>
+  </v-main>
 </template>
